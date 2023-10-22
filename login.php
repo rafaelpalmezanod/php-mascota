9bc70ca6cea464572ae88ef4cbbadcb273c90a6f
Rafael Andres Palmezano Diaz <rafaelandrespalmezanodias@gmail.com>
2023-10-21 21:52:28 -0500

    cambios

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <label for="username">Username</label>
+    <input type=" text" id="username">
+
+    <label for="password">Password</label>
+    <input type=" password" id="password">
+
+    <div class="remember">
+        <p>Recordar</p>
+        <p>Olvide Contraseña</p>
+    </div>
+
+    <input type="submint" class="btn-1" value="Iniciar Sesion">
+
+</body>
+</html>
