ac87c643ecce93a0643e68301a9b3f0ceb6efb32
Rafael Andres Palmezano Diaz <rafaelandrespalmezanodias@gmail.com>
2023-11-02 22:11:11 -0500

    DASHBOARD

---
+++ formulario.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <h1>Dashboard de Usuarios</h1>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nombre</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Password</th>
+          <th>Role ID</th>
+          <th>Editar</th>
+          <th>Eliminar</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Juan Pérez</td>
+          <td>juanperez</td>
+          <td>juanperez@example.com</td>
+          <td>123456</td>
+          <td>1</td>
+          <td><a href="#">Editar</a></td>
+          <td><a href="#">Eliminar</a></td>
+        </tr>
+     
+      </tbody>
+    </table>
+
+    
+</body>
+</html>
